21b98d3bf3bb48c9c1323bbcdd2283a2696df72d
Abdurasulova Gigi <giiiigiiii22@yandex.ru>
2021-10-03 19:24:54 +0300

    taask1
    taask1

---
+++ task1/task1.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+void main(void)
+{
+
+    double x = 20;
+    printf("x=%.4lf\n", x);
+    double f = 2 * pow(sin((3 * M_PI - 2 * x)), 2) * pow(cos(5 * M_PI + 2 * x), 2);
+    printf("f=%.4lf\n", f);
+    printf("x=");
+    scanf("%lf", &x);
+    f = 2 * pow(sin((3 * M_PI - 2 * x)), 2) * pow(cos(5 * M_PI + 2 * x), 2);
+    printf("f=%.4lf\n", f);
+
+}
